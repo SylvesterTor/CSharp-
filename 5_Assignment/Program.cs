e53f8b0903c5574ac6b926f3261ace3459f3b2f8
Sylvester Lave <Sylvester.lave@gmail.com>
2022-08-15 10:31:21 +0200

    Assignment 5

--- 5_Assignment/Program.cs
+++ 5_Assignment/Program.cs
@@ -1,21 +1,21 @@
 ﻿//Un-comment below code and fix the issues
 //Push to github
-/*
+
 int RecursiveFibonacci(int first, int second, int count)
 {
     count--;
     
     
     if(count == 0) {
-    
+        return first;
     }else{
-        return RecursiveFibonacci(?, ?, ?);
+        return RecursiveFibonacci(second, second+first, count);
     }
 }
 
 int lastFibo = RecursiveFibonacci(1,1,5);
 Console.WriteLine("Last fibo number was " + lastFibo);
 Console.ReadLine(); 
-*/
+
 
 
